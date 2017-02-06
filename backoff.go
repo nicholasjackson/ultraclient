f2627542dfa5601c445842ff986669e2da993e33
Nic Jackson <jackson.nic@gmail.com>
2017-02-06 10:06:57 +0000

    Renamed package

--- backoff.go
+++ backoff.go
@@ -1,4 +1,4 @@
-package loadbalancer
+package ultraclient
 
 // Backoff is an interface which defines the backoff algorythim
 // used by the retrier.
--- random_loadbalancing_test.go
+++ random_loadbalancing_test.go
@@ -1,4 +1,4 @@
-package loadbalancer
+package ultraclient
 
 import (
 	"net/url"
